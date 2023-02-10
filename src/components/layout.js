55f652d9f0ba7cbcc77011ba410425a3b2412872
Abidullah786 <abidullah.bsef18@iba-suk.edu.pk>
2023-02-10 15:00:06 +0500

    nav check for error

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './navBar';
 
 const PageLayout = () => {
   const [changeTheme, setChangeTheme] = useState(true);
@@ -8,7 +7,7 @@ const PageLayout = () => {
   return(
   
   <>
-    <Navbar selectedTheme={changeTheme} setSeletedTheme={setChangeTheme}/>
+    {/* <Navbar selectedTheme={changeTheme} setSeletedTheme={setChangeTheme}/> */}
     <Outlet /> 
   </>
 )};
